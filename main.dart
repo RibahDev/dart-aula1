d5a2679e9b889917d4e3d42e3a4681ff42265165
ribahdev <joao.ribeiro17@outlook.com>
2024-07-03 22:30:05 -0300

    Iniciando em dart

---
+++ main.dart
@@ -0,0 +1,16 @@
+main() {
+    
+    
+    String variavelNome = 'João';
+    print(variavelNome);
+
+    int valorVariavel = 10;
+    print(valorVariavel);
+    
+    bool ehVerdadeiro = true;
+    print(ehVerdadeiro);
+
+    List<String> listaDePalavras = ['João', 'Vitor'];
+    print(listaDePalavras[0]);
+
+}
