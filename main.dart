04fbc1a7d66ba62e2733a7ee05c9549be7e198fe
ribahdev <joao.ribeiro17@outlook.com>
2024-07-06 09:54:24 -0300

    Utilizando classes e metodos em Dart

--- main.dart
+++ main.dart
@@ -15,4 +15,33 @@ main() {
     List<String> listaDePalavras = ['João', 'Vitor'];
     print(listaDePalavras[1]);
 
+    Celular celularDoJoao = Celular('Azul', 5, 0.500, 5.7);
+    Celular celularDoZe = Celular('Preto', 8, 0.650, 6);
+
+    print(celularDoJoao.toString());
+    print(celularDoZe.toString());
+    double resultado = celularDoJoao.valorDoCelular(1000);
+    print(resultado);
+
+}
+
+
+
+// Criando classe
+
+class Celular { 
+  final String cor;
+  final int qtdPros;
+  final double tamanho;
+  final double peso;
+
+  Celular(this.cor, this.qtdPros, this.peso, this.tamanho);
+
+  String toString() {
+    return 'Cor: $cor, Quantidade de Processadores: $qtdPros, Tamanho: $tamanho, Peso: $peso g';
+  }
+
+  double valorDoCelular(double valor) {
+    return valor * qtdPros;
+  }
 }
