b12b8090b7d017ff1ce6bdf188997376d781c621
ribahdev <joao.ribeiro17@outlook.com>
2024-07-08 18:42:18 -0300

    Future, Async e Await

--- aula-2/main.dart
+++ aula-2/main.dart
@@ -1,5 +1,22 @@
-void main() {
+void main() async{
    
   //Future, Async e Await
 
+  String nome = 'João';
+  Future<String> cepFuture = getCepByName('Rua mariana');
+  late String cep;
+
+  //cepFuture.then((result) => cep = result);
+  //Await faz com que o código prossiga apenas após concluir a future
+  cep = await cepFuture;
+
+  print(cep);
+
+
+}
+
+//External service
+Future<String> getCepByName(String name) {
+  //Simulando requisição
+  return Future.value('4356465');
 }
